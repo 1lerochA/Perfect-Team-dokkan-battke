369436e86e614f762a64915adbc008388eb50250
A.leroch <alexislerocheleuil@gmail.com>
2026-03-23 15:33:48 +0100

    ajoute une page pour les quipes du moment

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 const navHTML = `
     <nav>
         <a href="index.html" id="link-index">🔮 Générateur</a>
+        <a href="metateams.html" id="link-metateams"> Équipes Méta</a>
         <a href="box.html" id="link-box">🎒 Ma Box</a>
         <a href="characters.html" id="link-characters">📖 Personnages</a>
         <a href="teams.html" id="link-teams">⭐ Mes Équipes</a>
@@ -18,6 +19,8 @@ if (currentPage.includes("box.html")) {
     document.getElementById('link-teams').classList.add('active');
 } else if (currentPage.includes("characters.html")) {
     document.getElementById('link-characters').classList.add('active');
+} else if (currentPage.includes("metateams.html")) {
+    document.getElementById('link-metateams').classList.add('active');
 } else {
     document.getElementById('link-index').classList.add('active');
 }
